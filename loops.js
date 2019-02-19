4d7c1d1773c68777f86e9bc694b87b109300721f
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-02-19 19:00:17 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,3 +1,9 @@
-function forLoop(ray) {}
+function forLoop(ray) {
+  for( var i = 1; i < 25 ; i++) {
+    ray[i - 1] = message
+  }
+}
+
 function whileLoop(number) {}
+
 function doWhileLoop()number {}
