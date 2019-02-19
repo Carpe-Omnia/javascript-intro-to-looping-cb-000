082f9800432709b6f409bea64de1cb76bfc94822
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-02-19 19:14:06 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -11,7 +11,7 @@ function forLoop(ray) {
 function whileLoop(number) {
   var number1 = number
   while (number1 >= 0) {
-    if (number1 > 0) {console.log(`#{number1}`)}
+    if (number1 > 0) {console.log(`${number1}`)}
     else {console.log("done")}
     number1 -= 1 ;
   }
