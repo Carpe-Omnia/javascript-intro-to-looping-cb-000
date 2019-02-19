5517a18815d5b058b7c46e95146b4fc19fbb22f7
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-02-19 19:07:34 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,7 +2,7 @@ function forLoop(ray) {
   var message
   for( let i = 0; i < 25 ; i++) {
     if (i === 0) {message = "I am 1 strange loop"}
-    else {message = `I am ${i + 1} strange loops`}
+    else {message = `I am ${i } strange loops`}
     ray.push(message)
   }
   return ray
